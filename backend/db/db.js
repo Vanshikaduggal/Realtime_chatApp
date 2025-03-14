49320044b98088fc29364c5ce06afedb9a807050
Vanshika <vanshikaduggal13@gmail.com>
2025-03-14 20:59:56 +0530

    login-logout completed

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,19 +1,18 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
-function connect() {
-  mongoose
-    .connect(process.env.MONGO_URI, {
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 5000,
-      socketTimeoutMS: 45000,
-    })
-    .then(() => {
-      console.log("Connected to MongoDB");
-    })
-    .catch((err) => {
-      console.error("MongoDB connection error:", err);
     });
-}
+    console.log('MongoDB Connected');
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
+    process.exit(1);
+  }
+};
 
 export default connect;
