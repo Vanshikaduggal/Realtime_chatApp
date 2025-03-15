200ee59bd242d89f120efd0ee8c658b3e81f7499
Vansh <vanshgarg2k5@gmail.com>
2025-03-15 15:08:52 +0530

    UI for login and register

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,81 +1,115 @@
-import userModel from "../models/user.model.js";
-import * as userService from '../services/user.service.js'
-import { validationResult } from "express-validator";
-import redisClient from "../services/redis.service.js";
+import userModel from '../models/user.model.js';
+import * as userService from '../services/user.service.js';
+import { validationResult } from 'express-validator';
+import redisClient from '../services/redis.service.js';
 
-export const createUserController = async(req,res)=>{
-    const errors = validationResult(req)
 
-    if(!errors.isEmpty())
-        return res.status(400).json({errors:errors.array()})
+export const createUserController = async (req, res) => {
 
-    try{
-        const user = await userService.createUser(req.body)
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+        const user = await userService.createUser(req.body);
+
         const token = await user.generateJWT();
-        res.status(201).json({ user, token }); // Fixed response format
-    } catch (error)
-    {
-        res.status(400).json({ error: error.message }); // Better error handling
+
+        delete user._doc.password;
+
+        res.status(201).json({ user, token });
+    } catch (error) {
+        res.status(400).send(error.message);
     }
 }
 
-export const loginController = async(req,res)=>{
-    const errors= validationResult(req);
+export const loginController = async (req, res) => {
+    const errors = validationResult(req);
 
-if(!errors.isEmpty())
-{
-    return res.status(400).json({errors:errors.array()});
-}
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
 
-try{
-    const {email,password}=req.body;
-    const user = await userModel.findOne({email}).select('+password');
-    if(!user)
-    {
-        res.status(401).json({
-            errors:'Invalid credentials'
-        })
-        const isMatch=await user.isValidPassword(password);
+    try {
 
-        if(!isMatch)
-        {
+        const { email, password } = req.body;
+
+        const user = await userModel.findOne({ email }).select('+password');
+
+        if (!user) {
             return res.status(401).json({
-                errors:'Invalid credentials'
+                errors: 'Invalid credentials'
+            })
+        }
+
+        const isMatch = await user.isValidPassword(password);
+
+        if (!isMatch) {
+            return res.status(401).json({
+                errors: 'Invalid credentials'
             })
         }
 
         const token = await user.generateJWT();
 
-        res.status(200).json({user,token})
-    }
-}
-catch(err)
-    {
-       res.status(400).send(err.message); 
+        delete user._doc.password;
+
+        res.status(200).json({ user, token });
+
+
+    } catch (err) {
+
+        console.log(err);
+
+        res.status(400).send(err.message);
     }
 }
 
-export const profileController =async(req,res)=>{
-    console.log(req.user);
+export const profileController = async (req, res) => {
 
     res.status(200).json({
-        user:req.user
-    })
+        user: req.user
+    });
+
 }
 
-export const logoutController =async(req,res)=>{
-    try{
-        const token = req.cookies.token || req.headers.authorization.split(' ')[1];
+export const logoutController = async (req, res) => {
+    try {
 
-        redisClient.set(token,'logout','EX',60*60*24);
+        const token = req.cookies.token || req.headers.authorization.split(' ')[ 1 ];
+
+        redisClient.set(token, 'logout', 'EX', 60 * 60 * 24);
 
         res.status(200).json({
-            message:'Logged out successfully'
-        })
-    }
-    catch(err)
-    {
+            message: 'Logged out successfully'
+        });
+
+
+    } catch (err) {
         console.log(err);
         res.status(400).send(err.message);
+    }
+}
+
+export const getAllUsersController = async (req, res) => {
+    try {
+
+        const loggedInUser = await userModel.findOne({
+            email: req.user.email
+        })
+
+        const allUsers = await userService.getAllUsers({ userId: loggedInUser._id });
+
+        return res.status(200).json({
+            users: allUsers
+        })
+
+    } catch (err) {
+
+        console.log(err)
+
+        res.status(400).json({ error: err.message })
+
     }
 }
